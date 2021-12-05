16bf742e20e4188bf587306dde43e73e1e839020
André Luis dos Santos <alsss.ads@gmail.com>
2021-12-05 15:48:51 -0300

    add user get and set

--- app/model/User.php
+++ app/model/User.php
@@ -18,4 +18,42 @@ class User{
 
     return $result;
   }
+
+
+
+  //get n set 
+  public function setlogin($login){
+    $this->login = $login;
+  }
+
+  public function getLogin(){
+    return $this->login;
+  }
+
+  public function setPassword($password){
+    $this->password = $password;
+  }
+
+  public function getPassword(){
+    return $this->password;
+  }
+
+
+  public function setEmail($email){
+    $this->email = $email;
+  }
+
+  public function getEmail(){
+    return $this->email;
+  }
+
+  public function setStatus($status){
+    $this->status = $status;
+  }
+
+  public function getStatus(){
+    return $this->status;
+  }
+
+
 }
